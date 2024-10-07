cc971baf31593c0f5cc7dd6658c1949260be3f3f
= <=>
2024-10-07 07:12:07 -0500

    rawrawrawr

--- flake.nix
+++ flake.nix
@@ -14,8 +14,8 @@
       version = "1.0";
 
       src = pkgs.fetchurl {
-        url = "https://objects.githubusercontent.com/github-production-release-asset-2e65be/868459808/dccf1faa-1833-4bd8-bece-a034cd582eec?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241006%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241006T164944Z&X-Amz-Expires=300&X-Amz-Signature=7b5592787a82d4ce7753560db420904f75622e2d1d7fb052d09fd839288d536f&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dtauri-media-player_0.1.0_amd64.deb&response-content-type=application%2Foctet-stream";
-        sha256 = "sha256-3TTOXVzykkXn58+wV/0t+Wv7Clpw2swfQKwVFcYnJps=";
+        url = "https://objects.githubusercontent.com/github-production-release-asset-2e65be/868459808/b6c3d24d-7819-4c03-bf3c-66f0de0333fd?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241007%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241007T121050Z&X-Amz-Expires=300&X-Amz-Signature=6cbe427c37b2517bd6484024049ca9605697b7e109d7b3299d6aa1516abce3a3&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dtauri-media-player_0.1.0_amd64.deb&response-content-type=application%2Foctet-stream";
+        sha256 = "sha256-/n0LFQaKbyLzzH5iy8XzG/3JEiCbEqUMvz6pKFiENDc=";
       };
 
       nativeBuildInputs = with pkgs; [
